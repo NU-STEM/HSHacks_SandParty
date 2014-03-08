af1f2680b52c6973f351dbfacecf30e2d9a40e61
Storm-Trooper-2 <davidbernadett@sbcglobal.net>
2014-03-08 14:45:54 -0800

    Added Frame --Alex

--- multiUniverse/src/main/MainProgram.java
+++ multiUniverse/src/main/MainProgram.java
@@ -1,7 +1,16 @@
 package main;
+
+import javax.swing.JFrame;
+
 //Alex
 //Can
 //Yoshi
 public class MainProgram {
-
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame();
+		myFrame.setVisible(true);
+		myFrame.setSize(500,500);
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
