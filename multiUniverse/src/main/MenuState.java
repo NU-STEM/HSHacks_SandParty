6790427c613b37c7e205c2992784a6582086f6b6
Storm_Trooper_4 <jmcdaniel@njuhsd.com>
2014-03-08 18:34:48 -0800

    added shit

--- multiUniverse/src/main/MenuState.java
+++ multiUniverse/src/main/MenuState.java
@@ -1,26 +1,45 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import main.StateManager;
+
 
 public class MenuState extends State implements ActionListener{
 	private boolean running = true;
 	int x = 0;
+	JButton btn = new JButton("Play");
+	JButton btn2 = new JButton("Quit");
 	@Override
 	public void activate(){
 		running = true;
-		this.add(btn);
-		btn.setActionCommand("Loop");
-		btn.addActionListener(this);
+		JFrame myFrame = new JFrame();
+		MenuState ms = new MenuState();
+		myFrame.add(ms);
+		myFrame.setSize(500, 500);
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setVisible(true);
+     	myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ms.setLayout(null);
 		
+	   
+	    
+	    btn.setFont(new Font("SansSerif", Font.ITALIC, 20));
+	    
+	    btn.addActionListener(this);
+	    btn.setActionCommand("Loop");
+		btn.addActionListener(this);
+	    btn.setBounds(190, 300, 120, 30);
+		ms.add(btn);
+	    
 		
 		
 		while(running){
@@ -32,7 +51,7 @@ public class MenuState extends State implements ActionListener{
 			}
 			x++;
 			this.repaint();
-			System.out.println("repaint called");
+			//System.out.println("repaint called");
 		}
 	}
 	public void deactivate(){
@@ -41,30 +60,35 @@ public class MenuState extends State implements ActionListener{
 		
 	}
 	
-	@Override
-	public void setUp(StateManager m){
+	
+	public void setup(StateManager m){
 		parent = m;
+		//this.setLayout(null);
 	}
 	StateManager parent = null;
-	JButton btn = new JButton("to Loop");
-	public void paint(Graphics g){
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.setColor(new Color(0.5f,0.0f,0.5f));
-		g2d.fillRect(x, 0, 20, 20);
-		System.out.println("Paint Called");
-	}
+	//JButton btn = new JButton("Play");
+	
+//	public void paint(Graphics g){
+//		super.paint(g);
+//		Graphics2D g2d = (Graphics2D)g;
+//		g2d.setColor(new Color(0.5f,0.0f,0.5f));
+//		g2d.fillRect(x, 0, 20, 20);
+//		
+		//System.out.println("Paint Called");
+//	}
 	public void paintComponent(Graphics g){
 		//super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
 		g2d.setColor(new Color(0.0f,1.0f,1.0f));
 		g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
 		System.out.println("Paint Component");
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if("Loop".equals(arg0.getActionCommand())){
-			parent.switchState("loop");
+			//parent.switchState("loop");
+			System.out.println("Loop Pressed!");
 		}
 		
 	}
